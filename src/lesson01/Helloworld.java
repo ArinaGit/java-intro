40d87f0b3cc3557bb140fdb02d3a037e121fcdae
Arina <test.ua>
2018-03-13 19:26:26 +0200

    comit

---
+++ src/lesson01/Helloworld.java
@@ -0,0 +1,20 @@
+package lesson01;
+
+public class Helloworld {
+    public static void main (String[] args) {
+       System.out.println("Hello world!");
+       boolean b = true;
+       int i = 2;
+       long l = 5000000000l;
+       float f = 9.14f;
+       double d = 34.32;
+       char c = 'F';
+       System.out.println(b);
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
